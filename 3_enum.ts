1594135dbb8412ae1602284d67ddb7f127757843
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-13 17:35:43 +0300

    enum, functions, classes

---
+++ 3_enum.ts
@@ -0,0 +1,18 @@
+enum Membership {
+    Simple,
+    Standard,
+    Premium
+}
+
+const membership = Membership.Standard
+const membershipRevers = Membership[2]
+console.log(membership)
+
+enum SocialMedia {
+    VK = 'VK',
+    FACEBOOK = 'FACEBOOK',
+    INSTAGRAM = 'INSTAGRAM'
+}
+
+const social = SocialMedia.INSTAGRAM
+console.log(social )
